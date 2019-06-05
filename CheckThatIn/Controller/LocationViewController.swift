3e18f6165df6f67defa7aebb38ff2c3f2e16de55
alongotv <pdastore1@gmail.com>
2019-06-05 13:57:48 +0300

    Add CoreLocation tracking code

---
+++ CheckThatIn/Controller/LocationViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  CheckThatIn
+//
+//  Created by Vladimir on 05/06/2019.
+//  Copyright © 2019 Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        requestPermissionToUseLocation()
+        startReceivingLocationChanges()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last as? CLLocation {
+            locationTextLabel.text = location.description
+            locationManager.stopUpdatingLocation()
+        }
+        // Do something with the location.
+    }
+    
+    func startReceivingLocationChanges() {
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        if authorizationStatus != .authorizedWhenInUse && authorizationStatus != .authorizedAlways {
+            // User has not authorized access to location information.
+            return
+        }
+        // Do not start services that aren't available.
+        if !CLLocationManager.locationServicesEnabled() {
+            // Location services is not available.
+            return
+        }
+        // Configure and start the service.
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100.0  // In meters.
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+    }
+
+    func requestPermissionToUseLocation() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse: do {
+            print("authorized")
+            startReceivingLocationChanges()            }
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("restricted")
+        case .denied:
+            print("denied")
+        @unknown default:
+            print("unknown")
+        }
+    }
+    
+    
+}
+
