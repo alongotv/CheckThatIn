4ac5132c55ba6d20fa7250292b9740cd4660501b
alongotv <pdastore1@gmail.com>
2019-06-16 22:13:23 +0300

    Add LocationModel struct

---
+++ CheckThatIn/Model/LocationModel.swift
@@ -0,0 +1,16 @@
+//
+//  LocationModel.swift
+//  CheckThatIn
+//
+//  Created by Vladimir Vetrov on 16/06/2019.
+//  Copyright © 2019 Vladimir. All rights reserved.
+//
+
+import Foundation
+
+struct LocationModel {
+    var latitude: String!
+    var longitude: String!
+    var dateCaptured: Date!
+    var descriptionToSave: String?
+}
