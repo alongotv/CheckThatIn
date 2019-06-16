5a9f4d573442bc4cbbc794afb001eaae39380b7e
alongotv <pdastore1@gmail.com>
2019-06-16 22:14:10 +0300

    Add LocationCollectionViewCell class

---
+++ CheckThatIn/View/LocationCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  LocationCollectionViewCell.swift
+//  CheckThatIn
+//
+//  Created by Vladimir Vetrov on 16/06/2019.
+//  Copyright © 2019 Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class LocationCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var labelLocationTitle: UILabel!
+    
+}
